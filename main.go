a2b25aa51b3a941bcd60764ed4038f18d7400511
codesoap <codesoap@mailbox.org>
2021-11-13 10:29:00 +0100

    wip

--- main.go
+++ main.go
@@ -5,25 +5,61 @@ import (
 	"crypto/x509"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
 )
 
+/*
+- Read header line. If MIME != text/* or encoding is not UTF-8: do something else
+- gmi is handled by the browser itself.
+- text is viewn in less?!
+- Read the tcp input into a buffer until \n is received.
+	- Discard \r if directly before \n.
+	- Replace control bytes by '�'.
+	- Limit buffer to 10k?!
+*/
+
 func main() {
+	u, err := getURL()
+	if err != nil {
+		panic(err)
+	}
 	conf := &tls.Config{
 		InsecureSkipVerify:    true,
 		VerifyPeerCertificate: verifyServersCert,
 	}
-	conn, err := tls.Dial("tcp", "gemini.circumlunar.space:1965", conf)
+	conn, err := tls.Dial("tcp", u.Host+":1965", conf)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	if _, err = conn.Write([]byte("gemini://gemini.circumlunar.space/docs/specification.gmi\r\n")); err != nil {
+	if _, err = conn.Write([]byte(u.String() + "\r\n")); err != nil {
 		panic(err)
 	}
 	if _, err = io.Copy(os.Stdout, conn); err != nil {
 		panic(err)
 	}
+}
+
+func getURL() (*url.URL, error) {
+	if len(os.Args) != 2 {
+		return &url.URL{}, fmt.Errorf("wrong argument count") // TODO: help text
+	}
+	u, err := url.Parse(os.Args[1])
+	if err != nil {
+		return &url.URL{}, err
+	} else if u.Host == "" {
+		u, err = url.Parse("//" + os.Args[1])
+		if err != nil {
+			return &url.URL{}, err
+		}
+	}
+	if u.Scheme == "" {
+		u.Scheme = "gemini"
+	} else if u.Scheme != "gemini" {
+		return &url.URL{}, fmt.Errorf("not a gemini URL")
+	}
+	return u, nil
 }
 
 func verifyServersCert(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
